58a911095dfc2359fb1e26a864bb34744d90fdb9
Lennart Mol <lc.mol@student.avans.nl>
2022-01-16 00:39:34 +0100

    Implemented first part of user input validation

--- Application/main.cpp
+++ Application/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <string>
 
-bool exit = false;
+std::string input;
+bool _exit = false;
 double latitude = 51.688307;
 double langitude = 5.286983;
 int slope = 40;
@@ -8,16 +10,29 @@ int orientation = -45;
 int power = 350;
 int energyUsage = 4000;
 
-int main(){
-    while(!exit)
+
+template <typename T>
+void checkInput(std::string input){
     
+    T data = std::stod(input);
+}
+
+/*template <typename T>
+void checkInput(std::string input, T type, int min, int max){
+    while(!(inputStream >> type)){
+        std::cout << "Wrong input type."  << std::endl;
+        std::cin.clear();
+        while (std::cin.get() != '\n') ;
+    }
+}*/
+
+int main()  {
     //enable when input
-    std::cout << "Enter Latitude (decimal degrees):" << std::endl;
-    std::cin >> latitude;
-    std::cout << "Enter langitude (decimal degrees):" << std::endl;
-    std::cin >> langitude;
+    std::cout << "Enter Latitude (degrees):" << std::endl;
+    std::cin >> input;
+    checkInput<double>(input);
+    std::cout << "Enter langitude (degrees):" << std::endl;
     std::cout << "Enter Fixed slope of modules (degrees) (min 0. max 90):" << std::endl;
-    std::cin >> slope;
     std::cout << "Enter Orientation (azimuth) of modules (degrees) (min -180. max 180.):" << std::endl;
     std::cin >> orientation;
     std::cout << "Enter Nominal power of the PV system (c-Si) (kWp) (min 0. max 500.): " << std::endl;
@@ -25,3 +40,4 @@ int main(){
     std::cout << "Enter energy usage (kWh/year) (min 0. max 20000.)" << std::endl;
     std::cin >> energyUsage;
 }   
+
