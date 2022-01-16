4b0bffda20c624d8cb5aaa054bc670ac7924c8b5
Lennart Mol <lc.mol@student.avans.nl>
2022-01-16 18:57:22 +0100

    finished user input validation

--- Application/main.cpp
+++ Application/main.cpp
@@ -1,43 +1,35 @@
 #include <iostream>
 #include <string>
 
-std::string input;
-bool _exit = false;
-double latitude = 51.688307;
-double langitude = 5.286983;
-int slope = 40;
-int orientation = -45;
-int power = 350;
+double latitude = 45.000;
+double langitude = 8.000;
+int peakPower = 1.0;
+const int loss = 14;
+int angle = 0;
+int aspect = 0;
 int energyUsage = 4000;
 
-
+// Check if input corresponds to the type of variable and to check if input is between min and max.
 template <typename T>
-void checkInput(std::string input){
-    
-    T data = std::stod(input);
-}
-
-/*template <typename T>
-void checkInput(std::string input, T type, int min, int max){
-    while(!(inputStream >> type)){
-        std::cout << "Wrong input type."  << std::endl;
+void checkInput(T variableType, int min, int max){
+    while(!(std::cin >> variableType) || !(variableType >= min && variableType <= max)){
+        std::cout << "Wrong input. Try again."  << std::endl;
         std::cin.clear();
         while (std::cin.get() != '\n') ;
     }
-}*/
+}
 
 int main()  {
-    //enable when input
     std::cout << "Enter Latitude (degrees):" << std::endl;
-    std::cin >> input;
-    checkInput<double>(input);
+    checkInput<double>(latitude, -90, 90);
     std::cout << "Enter langitude (degrees):" << std::endl;
-    std::cout << "Enter Fixed slope of modules (degrees) (min 0. max 90):" << std::endl;
-    std::cout << "Enter Orientation (azimuth) of modules (degrees) (min -180. max 180.):" << std::endl;
-    std::cin >> orientation;
+    checkInput<double>(langitude, -180, 180);
     std::cout << "Enter Nominal power of the PV system (c-Si) (kWp) (min 0. max 500.): " << std::endl;
-    std::cin >> power;
+    checkInput<int>(peakPower, 0, 500);
+    std::cout << "Enter Fixed slope of modules (degrees) (min 0. max 90):" << std::endl;
+    checkInput<int>(angle, 0, 90);
+    std::cout << "Enter Orientation (azimuth) of modules (degrees) (min -180. max 180.):" << std::endl;
+    checkInput<int>(aspect, -180, 180);
     std::cout << "Enter energy usage (kWh/year) (min 0. max 20000.)" << std::endl;
-    std::cin >> energyUsage;
+    checkInput<int>(energyUsage, 0, 20000);
 }   
-
